6db46b2570be60826fe5eecacf9d3d4f32ef018a
malja <jan@malcakov.cz>
2018-03-23 16:48:58 +0100

    Better tests
    More readable tests

--- zroya/python/tests/test_init.py
+++ zroya/python/tests/test_init.py
@@ -4,96 +4,26 @@ import unittest
 class zroya_init(unittest.TestCase):
 
     def test_NoParam(self):
-        """
-        Check that init function does require some parameters.
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.init()
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, lambda: zroya.init())
 
     def test_FewParams(self):
-        """
-        Check init function behavior when too few parameters is provided.
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.init("a", "b")
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, lambda: zroya.init("a", "b"))
 
     def test_FewParamsKeywords(self):
-        """
-        Init module with keywoards, but omnit some.
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.init(app_name="a", company_name="b")
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, lambda: zroya.init(app_name="a", company_name="b"))
 
     def test_EmptyParams(self):
-        """
-        Make sure init function requires non-empty strings
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.init("", "", "", "", "")
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, lambda: zroya.init("", "", "", "", ""))
 
     def test_EmptyParamsKeywoards(self):
-        """
-        Make sure init function requires non-empty strings
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.init(app_name="", company_name="", product_name="", sub_product="", version="")
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
-
-
+        self.assertRaises(ValueError, lambda: zroya.init(
+            app_name="", company_name="", product_name="", sub_product="", version="")
+        )
 
     def test_ProperInit(self):
-        """
-        Init module
-        true
-        """
-
         self.assertTrue(zroya.init("a", "b", "c", "d", "e"))
 
     def test_ProperInitKeywords(self):
-        """
-        Init module with keywords
-        true
-        """
-
         self.assertTrue(zroya.init(app_name="a", company_name="b", product_name="c", sub_product="d", version="e"))
 
 if __name__ == "__main__":
--- zroya/python/tests/test_show.py
+++ zroya/python/tests/test_show.py
@@ -1,66 +1,27 @@
 import zroya
 import unittest
 
+
 def on_click(id):
     pass
+
 
 class zroya_show(unittest.TestCase):
 
     def test_NoParam(self):
-        """
-        Show function requires some parameters.
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.show()
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, zroya.show())
 
     def test_FewParams(self):
-        """
-        Show function requires template parameter.
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.show( on_click=on_click)
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, lambda: zroya.show(on_click=on_click))
 
     def test_BadType(self):
-        """
-        Show function requires template parameter to be of zroya.Template type
-        exception
-        """
-
-        failed = False
-
-        try:
-            zroya.show( None )
-        except Exception as e:
-            failed = True
-
-        self.assertTrue(failed)
+        self.assertRaises(ValueError, lambda: zroya.show(None))
 
     def test_ProperParams(self):
-        """
-        Proper call of show function
-        integer
-        """
-
         t = zroya.Template(zroya.Template.TYPE_IMAGE_TEXT1 )
         t.firstLine("Test")
+        self.assertIsInstance(zroya.show(t), int)
 
-        self.assertIsInstance( zroya.show(t), int)
 
 if __name__ == "__main__":
     unittest.main()
