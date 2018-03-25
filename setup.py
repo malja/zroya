701f2f5140115421064112fe5828c425539dc403
malja <looorin@gmail.com>
2018-03-25 04:19:22 +0200

    Include current stub file
    Setup.py now include current stubs with installation.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, Extension
-import shutil
 import os
 
 includes_list = ["./zroya/include", "./zroya/include/python" ]
@@ -9,16 +10,27 @@ for root, dirs, files in os.walk("./zroya"):
         if os.path.splitext(f)[1] == ".cpp":
             sources_list.append(os.path.join(root, f))
 
-ext_modules = [Extension("zroya", sources = sources_list, include_dirs= includes_list)]
+ext_modules = [
+    Extension("zroya",
+              sources=sources_list,
+              include_dirs=includes_list,
+              extra_compile_args=[
+                  "/utf-8"
+              ]
+    )
+]
 
 for e in ext_modules:
     e.cython_directives = {"embedsignature": True}
 
 setup(name='zroya',
-      version='0.2.0',
-      description='Python implementation of Windows notifications.',
-      author='Jan Malčák',
-      author_email='jan@malcakov.cz',
-      url='https://github.com/malja/zroya',
-      ext_modules=ext_modules
+    version='0.2.0',
+    description='Python implementation of Windows notifications.',
+    author='Jan Malcak',
+    author_email='jan@malcakov.cz',
+    url='https://github.com/malja/zroya',
+    data_files=[
+      (".", ["./zroya/python/zroya.pyi"])
+    ],
+    ext_modules=ext_modules
 )
