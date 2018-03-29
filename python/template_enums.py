17ff096bd9d98f46d2aa7ff8d3c7c33bd8ed66c1
malja <jan@malcakov.cz>
2018-03-29 23:25:49 +0200

    Better documentation

--- python/template_enums.py
+++ python/template_enums.py
@@ -1,15 +1,22 @@
 from enum import IntEnum, Enum
 
 
-class AudioMode(IntEnum):
+class AudioMode(object):
+    """
+    Values for _mode_ parameter of zroya.Template.audio method.
+    Usage: template.audio(mode=zroya.AudioMode.Silence)
+    """
+
     Default = 0
     """
     Use default audio mode. Selected audio will be played only once.
     """
+
     Silence = 1
     """
     No audio is played at all.
     """
+
     Loop = 2
     """
     Play audio in loop until it is moved to Action Center. This time may vary due to different user configuration.
@@ -17,6 +24,11 @@ class AudioMode(IntEnum):
 
 
 class Audio(Enum):
+    """
+    This enumeration contains all accepted values for parameter _audio_ of zroya.Template.audio method.
+    Usage: template.audio(audio=zroya.Audio.IM)
+    """
+
     Default = "ms-winsoundevent:Notification.Default"
     IM = "ms-winsoundevent:Notification.IM"
     Mail = "ms-winsoundevent:Notification.Mail"
@@ -45,14 +57,47 @@ class Audio(Enum):
 
 
 class TemplateType(IntEnum):
-    ImageAndText1 = 0
+    """
+    All possible values for zroya.Template constructor.
+    Usage: zroya.Template(zroya.TemplateType.ImageAndText2)
     """
 
+    ImageAndText1 = 0
     """
+    A large image and a single string wrapped across three lines of text.
+    """
+
     ImageAndText2 = 1
-    ImageAndText3 = 3
-    ImageAndText4 = 4
-    Text1 = 5
-    Text2 = 6
-    Text3 = 7
-    Text4 = 8
+    """
+    A large image, one string of bold text on the first line, one string of regular text wrapped across the second and third lines.
+    """
+
+    ImageAndText3 = 2
+    """
+    A large image, one string of bold text wrapped across the first two lines, one string of regular text on the third line.
+    """
+
+    ImageAndText4 = 3
+    """
+    A large image, one string of bold text on the first line, one string of regular text on the second line, one string of regular text on the third line.
+    """
+
+    Text1 = 4
+    """
+    Single string wrapped across three lines of text.
+    """
+
+    Text2 = 5
+    """
+    One string of bold text on the first line, one string of regular text wrapped across the second and third lines.
+    """
+
+    Text3 = 6
+    """
+    One string of bold text wrapped across the first two lines, one string of regular text on the third line.
+    """
+
+    Text4 = 7
+    """
+    One string of bold text on the first line, one string of regular text on the second line, one string of regular text on the third line.
+    """
