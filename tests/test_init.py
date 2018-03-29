611ab728c46f8ccba2c4b93c0278d06d6144e048
malja <jan@malcakov.cz>
2018-03-29 23:27:39 +0200

    Rename import
    Revert wrong rename of zroya package.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,30 +1,30 @@
-import python
+import zroya
 import unittest
 
 class zroya_init(unittest.TestCase):
 
     def test_NoParam(self):
-        self.assertRaises(ValueError, lambda: python.init())
+        self.assertRaises(ValueError, lambda: zroya.init())
 
     def test_FewParams(self):
-        self.assertRaises(ValueError, lambda: python.init("a", "b"))
+        self.assertRaises(ValueError, lambda: zroya.init("a", "b"))
 
     def test_FewParamsKeywords(self):
-        self.assertRaises(ValueError, lambda: python.init(app_name="a", company_name="b"))
+        self.assertRaises(ValueError, lambda: zroya.init(app_name="a", company_name="b"))
 
     def test_EmptyParams(self):
-        self.assertRaises(ValueError, lambda: python.init("", "", "", "", ""))
+        self.assertRaises(ValueError, lambda: zroya.init("", "", "", "", ""))
 
     def test_EmptyParamsKeywoards(self):
-        self.assertRaises(ValueError, lambda: python.init(
+        self.assertRaises(ValueError, lambda: zroya.init(
             app_name="", company_name="", product_name="", sub_product="", version="")
         )
 
     def test_ProperInit(self):
-        self.assertTrue(python.init("a", "b", "c", "d", "e"))
+        self.assertTrue(zroya.init("a", "b", "c", "d", "e"))
 
     def test_ProperInitKeywords(self):
-        self.assertTrue(python.init(app_name="a", company_name="b", product_name="c", sub_product="d", version="e"))
+        self.assertTrue(zroya.init(app_name="a", company_name="b", product_name="c", sub_product="d", version="e"))
 
 if __name__ == "__main__":
     unittest.main()
