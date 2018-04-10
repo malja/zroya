4c5360d8f589d5e868ebf53a417263279aaa1253
malja <jan@malcakov.cz>
2018-04-11 00:51:33 +0200

    Call user callbacks from EventHandler

--- module/event_handler.cpp
+++ module/event_handler.cpp
@@ -2,13 +2,148 @@
 
 void zroya::EventHandler::toastActivated(INT64 notificationID) const {
     std::cout << "Cpp: EventHandler: Activated: " << notificationID << std::endl;
+	
+	auto it = this->callbacks.find(notificationID);
+	if (it != this->callbacks.end()) {
+
+		std::cout << "Cpp: EventHandler: Activated: Event found" << std::endl;
+
+		PyObject *onClicked = it->second->clicked;
+
+		if (onClicked) {
+
+			std::cout << "Cpp: EventHandler: Activated: Event handler is set" << std::endl;
+
+			if (PyCallable_Check(onClicked)) {
+
+				std::cout << "Cpp: EventHandler: Activated: Event handler is callable" << std::endl;
+				
+				PyGILState_STATE gstate;
+				gstate = PyGILState_Ensure();
+
+				PyObject *args = Py_BuildValue("(L)", notificationID);
+
+				PyObject_CallObject(onClicked, args);
+
+				PyGILState_Release(gstate);
+			}
+		}
+	}
 }
+
 void zroya::EventHandler::toastActivated(int actionIndex, INT64 notificationID) const {
 	std::cout << "Cpp: EventHandler: Activated2: " << notificationID << std::endl;
+
+	auto it = this->callbacks.find(notificationID);
+	if (it != this->callbacks.end()) {
+
+		std::cout << "Cpp: EventHandler: Activated2: Event found" << std::endl;
+
+		PyObject *onAction = it->second->action;
+
+		if (onAction) {
+
+			std::cout << "Cpp: EventHandler: Activated2: Event handler is set" << std::endl;
+
+			if (PyCallable_Check(onAction)) {
+
+				std::cout << "Cpp: EventHandler: Activated2: Event handler is callable" << std::endl;
+
+				PyGILState_STATE gstate;
+				gstate = PyGILState_Ensure();
+
+				PyObject *args = Py_BuildValue("(L, i)", notificationID, actionIndex);
+
+				PyObject_CallObject(onAction, args);
+
+				PyGILState_Release(gstate);
+			}
+		}
+	}
 }
+
 void zroya::EventHandler::toastDismissed(WinToastLib::IWinToastHandler::WinToastDismissalReason state, INT64 notificationID) const {
 	std::cout << "Cpp: EventHandler: Dismiss: " << notificationID << std::endl;
+
+	auto it = this->callbacks.find(notificationID);
+	if (it != this->callbacks.end()) {
+
+		std::cout << "Cpp: EventHandler: Dismiss: Event found" << std::endl;
+
+		PyObject *onDismiss = it->second->dismissed;
+
+		if (onDismiss) {
+
+			std::cout << "Cpp: EventHandler: Dismiss: Event handler is set" << std::endl;
+
+			if (PyCallable_Check(onDismiss)) {
+
+				std::cout << "Cpp: EventHandler: Dismiss: Event handler is callable" << std::endl;
+
+				PyGILState_STATE gstate;
+				gstate = PyGILState_Ensure();
+
+				PyObject *zroya_module = PyImport_ImportModule("zroya");
+				if (!zroya_module) {
+					std::cout << "no zroya module" << std::endl;
+					return;
+				}
+
+				PyObject *zroya_DissmissReason = PyObject_GetAttrString(zroya_module, "DismissReason");
+				if (!zroya_DissmissReason) {
+					std::cout << "No dismiss reason class" << std::endl;
+					return;
+				}
+
+				PyObject* zroya_DismissReason_instance = PyObject_CallFunction(zroya_DissmissReason, "i", (int)state);
+				PyObject *args = Py_BuildValue("(L, O)", notificationID, zroya_DismissReason_instance);
+				//PyObject *args = Py_BuildValue("(L, i)", notificationID, (int)state );
+
+				PyObject_CallObject(onDismiss, args);
+
+				PyGILState_Release(gstate);
+			}
+		}
+	}
 }
+
 void zroya::EventHandler::toastFailed(INT64 notificationID) const {
 	std::cout << "Cpp: EventHandler: Failed: " << notificationID << std::endl;
+
+	auto it = this->callbacks.find(notificationID);
+	if (it != this->callbacks.end()) {
+
+		std::cout << "Cpp: EventHandler: Failed: Event found" << std::endl;
+
+		PyObject *onFailed = it->second->failed;
+
+		if (onFailed) {
+
+			std::cout << "Cpp: EventHandler: Failed: Event handler is set" << std::endl;
+
+			if (PyCallable_Check(onFailed)) {
+
+				std::cout << "Cpp: EventHandler: Failed: Event handler is callable" << std::endl;
+
+				PyGILState_STATE gstate;
+				gstate = PyGILState_Ensure();
+
+				PyObject *args = Py_BuildValue("(L)", notificationID);
+
+				PyObject_CallObject(onFailed, args);
+
+				PyGILState_Release(gstate);
+			}
+		}
+	}
+}
+
+void zroya::EventHandler::addCallbacks(INT64 notificationID, PyObject *clicked, PyObject *action, PyObject *dismissed, PyObject *failed) {
+
+    this->callbacks.insert(
+		std::pair<INT64, Event*>(
+			notificationID, 
+			new Event(clicked, action, dismissed, failed)
+		)
+	);
 }
--- module/event_handler.h
+++ module/event_handler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "wintoastlib.h"
+#include <Python.h>
 
 namespace zroya {
 
@@ -10,9 +11,54 @@ namespace zroya {
 	 * If so, it is called.
 	 */
     class EventHandler : public WinToastLib::IWinToastHandler {
-        void toastActivated(INT64 notificationID) const;
-        void toastActivated(int actionIndex, INT64 notificationID) const;
-        void toastDismissed(WinToastDismissalReason state, INT64 notificationID) const;
-        void toastFailed(INT64 notificationID) const;
+
+        enum EventType {
+            Activated,
+            Action,
+            Dismissed,
+            Failed
+        };
+
+		class Event {
+			
+			public: 
+				PyObject *clicked;
+				PyObject *action;
+				PyObject *failed;
+				PyObject *dismissed;
+
+				Event(PyObject *clicked, PyObject *action, PyObject *dismissed, PyObject *failed) {
+					this->clicked = clicked;
+					this->action = action;
+					this->dismissed = dismissed;
+					this->failed = failed;
+				}
+
+				~Event() {
+					Py_XDECREF(this->clicked);
+					Py_XDECREF(this->action);
+					Py_XDECREF(this->dismissed);
+					Py_XDECREF(this->failed);
+				}
+
+		};
+
+        public:
+            void toastActivated(INT64 notificationID) const;
+            void toastActivated(int actionIndex, INT64 notificationID) const;
+            void toastDismissed(WinToastDismissalReason state, INT64 notificationID) const;
+            void toastFailed(INT64 notificationID) const;
+
+            void addCallbacks(INT64 notificationID, PyObject *activated, PyObject *action = nullptr, PyObject *dismissed = nullptr, PyObject *failed = nullptr);
+
+			~EventHandler() {
+				for (auto it = callbacks.begin(); it != callbacks.end(); it++) {
+					delete it->second;
+				}
+			}
+            
+        protected:
+
+            std::map<INT64, Event*> callbacks;
     };
 }
