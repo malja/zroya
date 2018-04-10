347bce199e4e01c1aafb15881034fd6058445467
malja <jan@malcakov.cz>
2018-04-11 00:58:20 +0200

    New callback test

---
+++ tests/test_callbacks.py
@@ -0,0 +1,20 @@
+import zroya
+import time
+
+def onclick(data):
+    print("Onclick event python handler {}".format(data))
+
+def ondismiss(data, reason):
+    print("Dismiss event python handler {}, reason {}".format(data, reason))
+    print(reason == zroya.DismissReason.User)
+
+zroya.init("python", "a", "b", "c", "d")
+
+t = zroya.Template(zroya.TemplateType.Text1)
+t.firstLine("Ahoj")
+
+
+d = zroya.show(t, on_click=onclick, on_dismiss=ondismiss)
+print("Status: {}".format(d))
+
+time.sleep(2)
